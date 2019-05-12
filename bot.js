b5a234566931668bf8b2f2ab2dd254751f041899
RonRon123 <50553285+RonRon123@users.noreply.github.com>
2019-05-12 21:26:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1674,4 +1674,4 @@ DiscordJS.Emoji.prototype.convertToString = function() {
 // Start Bot
 //---------------------------------------------------------------------
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.TOKEN);
